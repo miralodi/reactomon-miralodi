af3eefb182e8b73dcccd082d755fc6e3b1d4b6ab
Lorya <mircsi.lodi@gmail.com>
2020-02-05 14:23:59 +0100

    Display pokemon images

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,13 +1,29 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { Card } from "react-bootstrap";
+import axios from "axios";
 
 export class Pokemon extends Component {
+  state = {
+    imgUrl: ""
+  };
+
+  componentDidMount() {
+    axios
+      .get(this.props.pokemon.url)
+      .then(res => this.setState({ imgUrl: res.data.sprites.front_default }));
+  }
 
   render() {
     return (
-      <div style={pokemonStyle}>
-        <p>{this.props.pokemon.name}</p>
-      </div>
+      <span style={{ display: "inline-block", padding: "20px" }}>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={this.state.imgUrl} />
+          <Card.Body>
+            <Card.Title>{this.props.pokemon.name}</Card.Title>
+          </Card.Body>
+        </Card>
+      </span>
     );
   }
 }
