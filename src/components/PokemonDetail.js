f30875f11f245c763565b85ca66e4f47106fe8e0
Lorya <mircsi.lodi@gmail.com>
2020-02-06 13:59:44 +0100

    Rewrite PokemonDetail to functional component

--- src/components/PokemonDetail.js
+++ src/components/PokemonDetail.js
@@ -1,52 +1,68 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-export class PokemonDetail extends Component {
-  state = {
-    pokemonData: [],
-    types: [],
-    stats: []
-  };
+const PokemonDetail = props => {
+  const [pokemonData, setPokemonData] = useState([]);
+  const [types, setTypes] = useState([]);
+  const [stats, setStats] = useState([]);
+  const [abilities, setAbilities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  componentDidMount() {
-    axios
-      .get(`https://pokeapi.co/api/v2/pokemon/${this.props.match.params.id}/`)
-      .then(res =>
-        this.setState({
-          pokemonData: res.data,
-          types: res.data.types,
-          stats: res.data.stats
-        })
-      );
-  }
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(`https://pokeapi.co/api/v2/pokemon/${props.match.params.id}/`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch.");
+        }
+        setIsLoading(false);
+        return response.json();
+      })
+      .then(res => {
+        setPokemonData(res.sprites.front_default);
+        setTypes(res.types);
+        setStats(res.stats);
+        setAbilities(res.abilities);
+      })
+      .catch(err => {
+        console.log(err);
+        setIsLoading(false);
+      });
+  }, []);
 
-  render() {
-    console.log(this.state.stats);
-    return (
+  let content = <p>Loading pokemons...</p>;
+
+  if (!isLoading && pokemonData && types && stats && abilities) {
+    content = (
       <div>
-        <h1>{this.state.pokemonData.name}</h1>
-        <p>Height: {this.state.pokemonData.height}</p>
-        <p>Weight: {this.state.pokemonData.weight}</p>
+        <h1>{pokemonData.name}</h1>
+        <p>Height: {pokemonData.height}</p>
+        <p>Weight: {pokemonData.weight}</p>
         <h1>Types: </h1>
         <div>
-          {this.state.types.map(type => (
+          {types.map(type => (
             <li key={type.type.name}>{type.type.name}</li>
           ))}
         </div>
         <h1>Stats: </h1>
         <div>
-          {this.state.stats.map(stat => (
+          {stats.map(stat => (
             <li key={stat.stat.name}>
               {stat.stat.name}: {stat.base_stat}
             </li>
+          ))}
+        </div>
+        <h1>Abilities: </h1>
+        <div>
+          {abilities.map(ability => (
+            <li key={ability.ability.name}>{ability.ability.name}</li>
           ))}
         </div>
       </div>
     );
   }
 
-  // this.props.pokemons.map(pokemon => (
-  //   <Pokemon key={pokemon.name} pokemon={pokemon}
-}
+  return content;
+};
 
 export default PokemonDetail;
