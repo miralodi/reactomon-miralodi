c1230cb6c707bd2109a554fd701853d1993cd971
Lorya <mircsi.lodi@gmail.com>
2020-02-06 13:27:07 +0100

    Rewrite TypeList to functional component

--- src/components/TypeList.js
+++ src/components/TypeList.js
@@ -1,21 +1,45 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Type from "./Type";
-import axios from "axios";
 
-class TypeList extends Component {
-  state = {
-    types: []
-  };
+const TypeList = props => {
+  const [types, setTypes] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  componentDidMount() {
-    axios
-      .get("https://pokeapi.co/api/v2/type")
-      .then(res => this.setState({ types: res.data.results }));
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("https://pokeapi.co/api/v2/type")
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch.");
+        }
+        setIsLoading(false);
+        return response.json();
+      })
+      .then(res => {
+        setTypes(res.results);
+      })
+      .catch(err => {
+        console.log(err);
+        setIsLoading(false);
+      });
+  }, []);
+
+  let content = <p>Loading types...</p>;
+
+  if (!isLoading && types) {
+    content = types.map(type => (
+      <Type key={type.name} type={type} />
+    ));
   }
 
-  render() {
-    return this.state.types.map(type => <Type key={type.name} type={type} />);
-  }
-}
+  return content;
+};
+
 
 export default TypeList;
+
+// if (!isLoading && pokemons) {
+//   content = pokemons.map(pokemon => (
+//     <Pokemon key={pokemon.name} pokemon={pokemon} />
+//   ));
+// }
